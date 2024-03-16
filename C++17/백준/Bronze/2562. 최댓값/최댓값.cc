b0d250ed556e39894207c7c258bf7f8f1de429c3
HJO <122510632+kaxadlec@users.noreply.github.com>
2024-03-17 00:04:24 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2562. 최댓값/최댓값.cc
@@ -0,0 +1,16 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int arr[9];
+
+    for (int i=0; i<9; i++){
+        cin >> arr[i];
+    }
+    cout << *max_element(arr, arr+9) << "\n";
+    cout << max_element(arr, arr+9)-arr+1 << "\n";
+}
+
