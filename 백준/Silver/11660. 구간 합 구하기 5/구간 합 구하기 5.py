90fa15c7003eb7e2e740be0b67ef425833bb8160
HJO <122510632+kaxadlec@users.noreply.github.com>
2024-04-13 01:12:09 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1096 ms, Memory: 105940 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+N, M = map(int, stdin.readline().split())
+board = [list(map(int, stdin.readline().split())) for _ in range(N)]
+dp = [[0] * (N + 1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + board[i-1][j-1] - dp[i - 1][j - 1]
+
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, stdin.readline().split())
+    print(dp[x2][y2] - (dp[x1-1][y2] + dp[x2][y1-1]) + dp[x1-1][y1-1])
