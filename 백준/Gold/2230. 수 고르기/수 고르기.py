f444789e03d39a4225c3c66bfb8a9951b9857375
HJO <122510632+kaxadlec@users.noreply.github.com>
2024-04-19 00:17:33 +0900

    [Gold V] Title: 수 고르기, Time: 172 ms, Memory: 35364 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -2,22 +2,16 @@ from sys import stdin
 
 N, M = map(int, stdin.readline().split())
 arr = [int(stdin.readline()) for _ in range(N)]
-arr.sort()
+arr.sort() 
 
 min_diff = float('INF')
 
-end_flag = 0
-pre_end_idx = 0
-for st_idx in range(N):
-    for end_idx in range(pre_end_idx, N+1):
-        if end_idx == N:
-            end_flag = 1
-            break
-        if arr[end_idx]-arr[st_idx] >= M:
-            pre_end_idx = end_idx
-            min_diff = min(min_diff, arr[end_idx]-arr[st_idx])
-            break
-    if end_flag == 1:
+end_idx = 0
+for st_idx in range(N):  
+    while end_idx < N and arr[end_idx] - arr[st_idx] < M:
+        end_idx += 1
+    if end_idx == N:
         break
+    min_diff = min(min_diff, arr[end_idx]-arr[st_idx])
 
 print(min_diff)
