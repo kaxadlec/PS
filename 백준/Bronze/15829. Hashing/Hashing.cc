bd339da4ac8c381d6c44f37ea297dc8e30a30982
OH HYEONJIN <122510632+kaxadlec@users.noreply.github.com>
2024-06-22 14:23:47 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,29 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int L;
+    string s;
+
+    cin >> L;
+    cin >> s;
+
+    long long H = 0;
+    long long r = 1;
+    long long a;
+    long long M = 1234567891;
+
+    for (int i=0; i<L; i++) {
+        a = s[i] - 'a' + 1;
+        H = (H + a*r) % M;
+        r = r * 31; 
+    }
+
+    cout << H << endl;
+
+    return 0;
+}
