6b0e85fede246f3eb67b4246c7f55b5ef0927ebd
오현진 <122510632+kaxadlec@users.noreply.github.com>
2025-03-23 15:40:10 +0900

    [Gold V] Title: 강의실, Time: 272 ms, Memory: 53220 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.py
+++ 백준/Gold/1374. 강의실/강의실.py
@@ -8,8 +8,8 @@ pq = []
 classes.sort(key=lambda x:x[1])
 for claxx in classes:
     _, st, en = claxx
-    if pq and pq[0][0] <= st:
+    if pq and pq[0] <= st:
         heapq.heappop(pq)
-    heapq.heappush(pq, (en, (st, en)))
-    
+    heapq.heappush(pq, en)
+
 print(len(pq))
